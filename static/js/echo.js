d1b3090ef899a4fa4db8f0782d063fa4232921cd
AstyuteChick <130003082+AstyuteChick@users.noreply.github.com>
2026-05-16 07:04:16 +0200

    echo.js unique stat handling now works

--- static/js/echo.js
+++ static/js/echo.js
@@ -4,42 +4,71 @@ const echoData = JSON.parse(echoDataEle.textContent);
 const substatRollsEle = document.querySelector("#substat-rolls-data");
 const substatRollsData = JSON.parse(substatRollsEle.textContent);
 
-if (!charData[state.selectedChar]){selectChar(`Aemeath`);}
 state["echoData"] = [null, null, null, null, null, null, null, null, null, null, null, null, null];
+state["usefulStats"] = [];
+state["pickedStats"] = [];
 
 elms["allEchoNameSlct"] = document.querySelectorAll(".statNameSlct");
 elms["allEchoValSlct"] = document.querySelectorAll(".statValueSlct");
 
 function setEchoEventListeners(){
-    elms["allEchoNameSlct"].forEach(function (slct){slct.addEventListener("change", handleEchoSlctChange);});
-    elms["allEchoValSlct"].forEach(function (slct){slct.addEventListener("change", handleEchoSlctChange);});
-    elms["charOptsLst"].addEventListener("click", setEchoSubstatNames);
+    elms["allEchoNameSlct"].forEach(function (slct){
+        slct.dataset.prevVal = slct.value;
+        slct.addEventListener("change", handleSubstatChange);
+    });
+    elms["allEchoValSlct"].forEach(function (slct){slct.addEventListener("change", handleSubstatChange);});
+    elms["charOptsLst"].addEventListener("click", renderSubstatNames);
 }
 
-function setEchoSubstatNames(){
-    elms["allEchoNameSlct"].forEach(function (nameSlct){nameSlct.innerHTML = "<option val = 'noVal'>Select Echo Substat</option>"});
+function renderSubstatNames(){
+    elms["allEchoNameSlct"].forEach(function (nameSlct){nameSlct.innerHTML = "<option value = 'noVal'>Select Echo Substat</option>"});
     charData[state.selectedChar][0].forEach(function (relVal, ind){
-        if (relVal) {
-            elms["allEchoNameSlct"].forEach(function (nameSlct){
-                const opt = document.createElement("option");
-                opt.value = echoData[ind];
-                opt.textContent = echoData[ind];
-                nameSlct.appendChild(opt);});
-        }
+        if (!relVal) return;
+        const curStat = echoData[ind];
+        state.usefulStats.push(curStat);
+        elms["allEchoNameSlct"].forEach(function (nameSlct){
+            const opt = document.createElement("option");
+            opt.value = curStat;
+            opt.textContent = curStat;
+            nameSlct.appendChild(opt);
+        });
     });
 }
 
-function handleEchoSlctChange(event){
+function updateSubstatOpts(){
     elms["allEchoNameSlct"].forEach(function (nameSlct){
-        console.log(event.currentTarget.value);
-        console.log(nameSlct.id, event.currentTarget.id);
-        if (nameSlct.id !== event.currentTarget.id && event.currentTarget.value !== "noVal"){
-            nameSlct.querySelector(`option[value='${event.currentTarget.value}']`).remove();
-        } else if (event.currentTarget.value === "noVal") {
-            // Nahh this is getting too weird. Need functions to handle adding and removing elements. 
-        }
+        const curVal = nameSlct.value;
+        // const prevVal = nameSlct.dataset.prevVal;
+        nameSlct.innerHTML = "<option value = 'noVal'>Select Echo Substat</option>";
+        state.usefulStats.forEach(function (stat){
+            if (stat === curVal) {
+                const opt = document.createElement("option");
+                opt.value = stat;
+                opt.textContent = stat;
+                nameSlct.appendChild(opt);
+            } else if (!state.pickedStats.includes(stat)) {
+                const opt = document.createElement("option");
+                opt.value = stat;
+                opt.textContent = stat;
+                nameSlct.appendChild(opt);
+            }
+        });
+        nameSlct.value = curVal;
     });
+}
+
+function handleSubstatChange(event){
+    const curSlct = event.currentTarget;
+    const prevVal = curSlct.dataset.prevVal;
+    curSlct.dataset.prevVal = curSlct.value;
+    if (curSlct.value !== "noVal") {
+        if (prevVal !== "noVal") {state.pickedStats.splice(state.pickedStats.indexOf(prevVal), 1);}
+        state.pickedStats.push(curSlct.value);
+    } else {
+        state.pickedStats.splice(state.pickedStats.indexOf(prevVal), 1);
+    }
+    updateSubstatOpts();
 }
 
 setEchoEventListeners();
-setEchoSubstatNames();
+renderSubstatNames();
