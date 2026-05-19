56377dc4510bf0fe5ad013129870e073350d4d14
AstyuteChick <130003082+AstyuteChick@users.noreply.github.com>
2026-05-19 02:48:17 +0200

    have to fix issue with substats not added/removed properly after team/er swap

--- static/js/full.js
+++ static/js/full.js
@@ -42,10 +42,6 @@ function renderFullSubstatNames(){
     });
 }
 
-function renderFullVals(){
-
-}
-
 function updateFullSubstats(){
     elms["allEchoesNameSlct"].forEach(function (nameSlct) {
         const statName=nameSlct.value;
@@ -60,7 +56,8 @@ function updateFullSubstats(){
             opt.textContent=stat;
             nameSlct.appendChild(opt);
         });
-        nameSlct.value=statName;
+        if (statName==="ER(%)" && !state["usefulStats"].includes("ER(%)")) {nameSlct.value="noVal";}
+        else {nameSlct.value=statName;}
     });
 }
 
@@ -70,22 +67,79 @@ function handleFullSubstatChange(event){
     const statName=statSlct.value;
     const prevStatName=statSlct.dataset.prevVal;
     statSlct.dataset.prevVal=statName;
+    console.log(statName, prevStatName);
     if (statName!=="noVal") {state["pickedStats"][echoInd].push(statName);}
     if (prevStatName!=="noVal") {state["pickedStats"][echoInd].splice(state["pickedStats"][echoInd].indexOf(prevStatName), 1);}
     updateFullSubstats();
 }
 
-function handleFullValChange(){
-
+function handleFullTeamChange(event){
+    const teamSlct=event.currentTarget;
+    state["fullData"].forEach(function (echo, ind) {state["fullData"][ind][12]=0;});
+    let erReq=0;
+    if (teamSlct.value!=="noVal") {erReq=charData[state["selectedChar"]][1][0][teamSlct.value];}
+    console.log(erReq);
+    if (erReq>100) {
+        if (!state["usefulStats"].includes("ER(%)")) {state["usefulStats"].push("ER(%)");}
+    } else {
+        if (state["usefulStats"].includes("ER(%)")) {state["usefulStats"].splice(state["usefulStats"].indexOf("ER(%)"), 1)}
+    }
+    updateFullSubstats();
 }
 
-function calcFullResults(){}
+function renderFullVals(event){
+    const uid="val-"+event.currentTarget.id.slice(-2);
+    const statName=event.currentTarget.value;
+    elms["allEchoesValSlct"].forEach(function (valSlct) {
+        if (uid!==valSlct.id) return;
+        valSlct.innerHTML=`<option value="noVal">Value</option>`;
+        if (statName==="noVal") return;
+        substatRollsData[statName].forEach(function (roll) {
+            const opt=document.createElement("option");
+            opt.value=roll;
+            opt.textContent=roll;
+            valSlct.appendChild(opt);
+        });
+    });
+}
+
+function handleFullValChange(event){
+    const valSlct=event.currentTarget;
+    if (valSlct.value==="noVal") return;
+    const uid=valSlct.id.slice(-2);
+    const statName=document.getElementById("stat-"+uid).value;
+    const statInd=echoData.indexOf(statName);
+    const statRoll=valSlct.value;
+    state["fullData"][Number(uid[0])-1][statInd]=statRoll;
+}
+
+async function calcFullResults(){
+    try {
+        const response = await fetch("/calcFull", {
+            method: "POST", 
+            headers: {"Content-Type": "application/json"}, 
+            body: JSON.stringify({
+                char: state.selectedChar, 
+                team: state.selectedTeam, 
+                totEr: state.totEr, 
+                ssr: state.fullData
+            })
+        });
+        if (!response.ok) {throw new Error("Server Error");}
+        const result = await response.json();
+        updateFullResults(result);
+    } catch (error) {
+        console.error("Submit Failed: ", error)
+    }
+}
 
 function setFullEventListeners(){
     elms["charOptsLst"].addEventListener("click", renderFullSubstatNames);
+    elms["teamSlct"].addEventListener("change", renderFullSubstatNames);
     elms["allEchoesNameSlct"].forEach(function (nameSlct) {
         nameSlct.dataset.prevVal=nameSlct.value;
         nameSlct.addEventListener("change", handleFullSubstatChange);
+        nameSlct.addEventListener("change", renderFullVals)
     });
     elms["allEchoesValSlct"].forEach(function (valSlct) {valSlct.addEventListener("change", handleFullValChange)})
     elms["form"].addEventListener("submit", calcFullResults);
