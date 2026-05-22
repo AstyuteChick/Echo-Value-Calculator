e1f734979ff8554e6466c0866f21a404abd29de6
AstyuteChick <130003082+AstyuteChick@users.noreply.github.com>
2026-05-23 01:30:24 +0200

    formatted and organized calcbasejs, next is echojs

--- static/js/calcBase.js
+++ static/js/calcBase.js
@@ -1,20 +1,20 @@
 
-const charDataEle = document.querySelector("#char-data");
-const charData = JSON.parse(charDataEle.textContent); //Character.data
+const charDataEle=document.querySelector("#char-data");
+const charData=JSON.parse(charDataEle.textContent); //Character.data
 
-const echoDataEle = document.querySelector("#echo-data");
-const echoData = JSON.parse(echoDataEle.textContent); //GameData.substat_names (ordered substat names, list)
-const substatRollsEle = document.querySelector("#substat-rolls-data");
-const substatRollsData = JSON.parse(substatRollsEle.textContent); //GameData.substat_rolls (dict -> statName: list of rolls)
+const echoDataEle=document.querySelector("#echo-data");
+const echoData=JSON.parse(echoDataEle.textContent); //GameData.substat_names (ordered substat names, list)
+const substatRollsEle=document.querySelector("#substat-rolls-data");
+const substatRollsData=JSON.parse(substatRollsEle.textContent); //GameData.substat_rolls (dict -> statName: list of rolls)
 
-const state = {
+const state={
     selectedChar: `Aemeath`, 
     isCharMenuOpen: false, 
     selectedTeam: null, 
     totEr: 100.0
-};
+}
 
-const elms = {
+const elms={
     form: document.querySelector(".calcForm"), 
     selectedChar: document.querySelector(".selectedChar"), 
     searchChar: document.querySelector("#char-search"), 
@@ -23,29 +23,31 @@ const elms = {
     totErIn: document.querySelector("#tot-er"), 
     scoreVal: document.querySelector(".scoreVal"), 
     tierVal: document.querySelector(".tierVal")
-};
+}
 
-function renderCharOpts(searchInp=""){
-    const normInp = searchInp.toLowerCase().trim();
-    const charNames = Object.keys(charData);
-    const filteredChars = charNames.filter(function(charName){return charName.toLowerCase().includes(normInp);});
-    if (filteredChars.length===0){
-        elms["charOptsLst"].innerHTML = `<li><span>No Characters Found</span></li>`;
+function handleSubmit(event) {event.preventDefault();}
+
+function renderCharOpts(searchInp="") {
+    const normInp=searchInp.toLowerCase().trim();
+    const charNames=Object.keys(charData);
+    const filteredChars=charNames.filter(function(charName) {return charName.toLowerCase().includes(normInp);});
+    if (filteredChars.length===0) {
+        elms["charOptsLst"].innerHTML=`<li><span>No Characters Found</span></li>`;
         return;
     }
-    elms["charOptsLst"].innerHTML = ``;
-    filteredChars.forEach(function (charName){
-        const li = document.createElement("li");
-        const btn = document.createElement("button");
-        const img = document.createElement("img");
-        const spn = document.createElement("span");
+    elms["charOptsLst"].innerHTML=``;
+    filteredChars.forEach(function (charName) {
+        const li=document.createElement("li");
+        const btn=document.createElement("button");
+        const img=document.createElement("img");
+        const spn=document.createElement("span");
         const cleanCharName=charName.split('(')[0].trim().split(' ')[0].trim();
-        spn.textContent = charName;
-        img.src = `${staticImgPath}Resonator_${cleanCharName}.webp`;
+        spn.textContent=charName;
+        img.src=`${staticImgPath}Resonator_${cleanCharName}.webp`;
         img.classList.add("charPortrait");
-        btn.type = "button";
+        btn.type="button";
         btn.classList.add("btn", "charOptsBtn");
-        btn.dataset.charName = charName;
+        btn.dataset.charName=charName;
         btn.appendChild(img);
         btn.appendChild(spn);
         li.appendChild(btn);
@@ -53,51 +55,50 @@ function renderCharOpts(searchInp=""){
     });
 }
 
-function renderTeamOpts(charName){
-    elms["teamSlct"].innerHTML = `<option value="noVal">Select your character's team</option>`;
-    const charTeams = Object.keys(charData[charName][1][0]);
-    charTeams.forEach(function (team){
-        const opt = document.createElement("option");
-        opt.value = team;
-        opt.textContent = team;
-        elms.teamSlct.appendChild(opt);
-    });
-    state["selectedTeam"] = null;
-    elms["teamSlct"].value = "noVal";
-}
-
-function handleSubmit(event){event.preventDefault();}
-
-function handleCharSearchFocus(){
-    if (elms["searchChar"].value) {elms["searchChar"].value = "";}
-    openCharMenu();
-}
-
-function openCharMenu(){
-    elms["charOptsLst"].hidden = false;
-    state["isCharMenuOpen"] = true;
+function openCharMenu() {
+    elms["charOptsLst"].hidden=false;
+    state["isCharMenuOpen"]=true;
     renderCharOpts(elms["searchChar"].value);
 }
 
-function closeCharMenu(){
-    elms["charOptsLst"].hidden = true;
-    elms["searchChar"].value = state.selectedChar;
-    state["isCharMenuOpen"] = false;
+function handleCharSearchFocus() {
+    elms["searchChar"].value="";
+    openCharMenu();
 }
 
-function handleCharSearchType(){renderCharOpts(elms["searchChar"].value);}
+function handleCharSearchType() {renderCharOpts(elms["searchChar"].value);}
 
-function selectChar(charName){
+function closeCharMenu() {
+    elms["charOptsLst"].hidden=true;
+    elms["searchChar"].value=state.selectedChar;
+    state["isCharMenuOpen"]=false;
+}
+
+function renderTeamOpts(charName) {
+    elms["teamSlct"].innerHTML=`<option value="noVal">Select your character's team</option>`;
+    elms["teamSlct"].classList.remove("hasVal");
+    const charTeams=Object.keys(charData[charName][1][0]);
+    charTeams.forEach(function (team) {
+        const opt=document.createElement("option");
+        opt.value=team;
+        opt.textContent=team;
+        elms.teamSlct.appendChild(opt);
+    });
+    state["selectedTeam"]=null;
+    elms["teamSlct"].value="noVal";
+}
+
+function selectChar(charName) {
     if (!charName) return;
-    state["selectedChar"] = charName;
-    elms["searchChar"].value = charName;
-    const img = document.createElement("img");
-    const spn = document.createElement("span");
+    state["selectedChar"]=charName;
+    elms["searchChar"].value=charName;
+    const img=document.createElement("img");
+    const spn=document.createElement("span");
     const cleanCharName=charName.split('(')[0].trim().split(' ')[0].trim();
-    img.src = `${staticImgPath}Resonator_${cleanCharName}.webp`;
+    img.src=`${staticImgPath}Resonator_${cleanCharName}.webp`;
     img.classList.add("charPortrait");
-    spn.textContent = charName;
-    elms["selectedChar"].innerHTML = ``;
+    spn.textContent=charName;
+    elms["selectedChar"].innerHTML=``;
     elms["selectedChar"].appendChild(img);
     elms["selectedChar"].appendChild(spn);
     closeCharMenu();
@@ -106,34 +107,32 @@ function selectChar(charName){
     elms["totErIn"].value="";
 }
 
-function handleCharSearchClick(event){
-    const clickedBtn = event.target.closest(".charOptsBtn");
+function handleCharSearchClick(event) {
+    const clickedBtn=event.target.closest(".charOptsBtn");
     if (!clickedBtn) return;
     selectChar(clickedBtn.dataset.charName);
 }
 
-function handleOutsideClick(event){
-    const clickInCharOpts = event.target.closest(".charOptsLst");
-    const clickInCharInp = event.target.closest(".charSearchInp");
+function handleOutsideClick(event) {
+    const clickInCharOpts=event.target.closest(".charOptsLst");
+    const clickInCharInp=event.target.closest(".charSearchInp");
     if (!clickInCharOpts && !clickInCharInp) {closeCharMenu();}
 }
 
-function handleTeamChange(event){
-    const slctTeamVal = event.target.value;
-    if (slctTeamVal === "noVal") {
-        state["selectedTeam"] = null;
+function handleTeamChange(event) {
+    const slctTeamVal=event.target.value;
+    if (slctTeamVal==="noVal") {
+        state["selectedTeam"]=null;
     } else {
-        state["selectedTeam"] = slctTeamVal;
+        state["selectedTeam"]=slctTeamVal;
     }
 }
 
-function handleTotErFocus(){
+function handleTotErFocus() {
     if (elms["totErIn"].value) {elms["totErIn"].value="";}
 }
 
-function handleTotErType(){
-    state["totEr"] = elms["totErIn"].value;
-}
+function handleTotErType() {state["totEr"]=elms["totErIn"].value;}
 
 function controlStyles(event) {
     const elem=event.currentTarget;
@@ -149,23 +148,20 @@ function controlStyles(event) {
     else {elem.classList.remove("hasVal");}
 }
 
-function setEventListeners(){
+function setEventListeners() {
     elms["form"].addEventListener("submit", handleSubmit);
     elms["searchChar"].addEventListener("focus", handleCharSearchFocus);
     elms["searchChar"].addEventListener("input", handleCharSearchType);
     elms["charOptsLst"].addEventListener("click", handleCharSearchClick);
+    document.addEventListener("click", handleOutsideClick);
     elms["teamSlct"].addEventListener("change", handleTeamChange);
     elms["totErIn"].addEventListener("focus", handleTotErFocus);
     elms["totErIn"].addEventListener("input", handleTotErType);
-    document.addEventListener("click", handleOutsideClick);
-    elms["searchChar"].addEventListener("input", controlStyles);
     elms["teamSlct"].addEventListener("change", controlStyles);
     elms["totErIn"].addEventListener("input", controlStyles);
 }
 
-function setup(){
-    selectChar(state["selectedChar"]);
-}
+function setup() {selectChar(state["selectedChar"]);}
 
 setEventListeners();
 setup();
