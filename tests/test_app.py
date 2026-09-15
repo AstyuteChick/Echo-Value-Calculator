0621966886b8e415ad88791db97fb7105a0ff215
AstyuteChick <130003082+AstyuteChick@users.noreply.github.com>
2026-09-15 12:01:15 +0200

    stage 5: integration tests 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,4 +17,25 @@ def test_calc_echo_success(client, valid_request_payload):
     response=client.post("/calcEcho", json=valid_request_payload)
     assert response.status_code==200
     assert response.get_json()=={"score": "0.0", "tier": "Not Applicable"}
-    assert response.content_type=="application/json" #not sure
+    assert response.content_type=="application/json"
+
+def test_calc_echo_malformed_json(client):
+    response = client.post("/calcEcho", data="{'char': 'Suisui',", content_type="application/json")
+    assert response.status_code==400
+    assert response.get_json()["code"]=="invalid_json"
+    assert response.is_json
+
+def test_json_invalid_contract(client, valid_request_payload):
+    valid_request_payload["char"]=123
+    response=client.post("/calcEcho", json=valid_request_payload)
+    assert response.status_code==400
+    assert response.get_json()["code"]=="invalid_request"
+    assert response.is_json
+
+def test_main_internal_error(client, valid_request_payload, monkeypatch):
+    def fake_main(*args, **kwargs): raise RuntimeError("internal_crash")
+    monkeypatch.setattr("evc_app.main", fake_main)
+    response=client.post("/calcEcho", json=valid_request_payload)
+    assert response.status_code==500
+    assert response.get_json()["code"]=="internal_error"
+    
